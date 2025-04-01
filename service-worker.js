e32bff726bf5d2a5a1f47f49b489ed8410c91222
A4Studio <a4studios04@gmail.com>
2025-04-01 16:16:27 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,28 @@
+const CACHE_NAME = "pwa-cache-v1";
+const CACHE_FILES = [
+    "/",
+    "/index.html",
+    "/style.css",
+    "/app.js",
+    "/manifest.json",
+    "/icon.png"
+];
+
 self.addEventListener("install", event => {
     event.waitUntil(
-        caches.open("pwa-cache").then(cache => {
-            return cache.addAll([
-                "/",
-                "/index.html",
-                "/style.css",
-                "/app.js",
-                "/manifest.json",
-                "/icon.png"
-            ]);
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(CACHE_FILES);
+        })
+    );
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.filter(name => name !== CACHE_NAME)
+                    .map(name => caches.delete(name))
+            );
         })
     );
 });
